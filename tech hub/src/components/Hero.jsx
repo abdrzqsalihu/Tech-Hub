426258d896dc1da82e6571d48bb2d285b1ffefeb
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-04-03 12:37:36 +0100

    commit

--- tech hub/src/components/Hero.jsx
+++ tech hub/src/components/Hero.jsx
@@ -4,9 +4,9 @@ import { hero, play } from '../assets';
 
 const Hero = () => {
   return (
-    <section className='flex flex-1 flex-col lg:flex-row md:pt-20 pb-0 p-5 mt-10'>
+    <section className='flex flex-1 flex-col lg:flex-row md:pt-20 pb-0 mt-10'>
         <div className={`${styles.heading2} mt-16`}>
-            <h1 style={{fontFamily: 'public sans'}}>
+            <h1 style={{fontFamily: 'public sans'}} className='text-[30px]'>
             Unleash Your <span style={{fontFamily: 'public sans', color: '#EEA10D'}}>Tech Skills</span> <br className='sm:block hidden'/> and Shape the Future <br className='sm:block hidden'/> with <span style={{fontFamily: 'forte', color: '#EEA10D'}}>Tech Hub</span>
             </h1>
             <h2 className={`${styles.paragraph} text-white`}>
@@ -19,7 +19,7 @@ const Hero = () => {
 
                 <div className='flex items-center'>
                   <img src={play} alt="" className='w-[15%]'/>
-                <a href="#" style={{fontFamily: 'poppins', fontSize: '18px', fontWeight: '100', marginLeft: '25px'}}>Watch how it works</a>
+                <a href="#" style={{fontFamily: 'poppins', fontWeight: '100', marginLeft: '25px'}}>Watch how it works</a>
                 </div>
             </div>
         </div>
